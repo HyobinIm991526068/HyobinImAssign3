0b103cc756b4134899c8fbd522e2f5c04e6d3797
ihb92 <jakeim9@gmail.com>
2021-03-27 16:44:02 -0400

    Commit #1 - Initial Commit

---
+++ HyobinAssign33/src/main/java/hyobin/im/s991526068/HyobinActivity.java
@@ -0,0 +1,21 @@
+/**
+ *  Assignment 3
+ *  Hyobin Im
+ *  991526068
+ *  Class: 1211_34780
+ */
+
+package hyobin.im.s991526068;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HyobinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ HyobinAssign33/src/main/java/hyobin/im/s991526068/S991526068Fragment.java
@@ -0,0 +1,70 @@
+/**
+ *  Assignment 3
+ *  Hyobin Im
+ *  991526068
+ *  Class: 1211_34780
+ */
+package hyobin.im.s991526068;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link S991526068Fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class S991526068Fragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public S991526068Fragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment S991526068Fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static S991526068Fragment newInstance(String param1, String param2) {
+        S991526068Fragment fragment = new S991526068Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_s991526068, container, false);
+    }
+}
